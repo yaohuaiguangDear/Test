d90d4b079f9e0c07680c0e091dcc131e317a3362
yao <yao@medishare.cn>
2018-02-02 15:50:10 +0800

    Initial Commit

---
+++ HuijieDeng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuijieDeng
+//
+//  Created by yao on 2018/2/2.
+//  Copyright © 2018年 yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuijieDeng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuijieDeng
+//
+//  Created by yao on 2018/2/2.
+//  Copyright © 2018年 yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
